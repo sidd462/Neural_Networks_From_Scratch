4f4a2a89b33b0713c378ca44c611bb6ba70a7562
sidd462 <siddgupta462@gmail.com>
2024-10-10 06:31:07 +0530

    simplified mathematical logic

--- p07.py
+++ p07.py
@@ -1,12 +1,14 @@
-import math
+import numpy as np
+
 
 softmax_output = [0.7,0.1,0.2]
 target_output = [1,0,0]
 target_class = 0
 
-#Categorical cross entropy loss
-loss = -(math.log(softmax_output[0])*target_output[0] + 
-        (math.log(softmax_output[1])*target_output[1]) + 
-        math.log(softmax_output[2])*target_output[2])
+#! Categorical cross entropy loss
+# loss = -(math.log(softmax_output[0])*target_output[0] + 
+#         (math.log(softmax_output[1])*target_output[1]) + 
+#         math.log(softmax_output[2])*target_output[2])
+loss = -np.sum(target_output * np.log(softmax_output))
 
 print(loss)
